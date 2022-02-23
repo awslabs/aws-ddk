79709822a27eaac5be74802966eb8b99df3fd5b0
jaidisido <jaidisido@gmail.com>
2022-02-23 10:09:30 +0000

    Fix wrong ddk core version in requirements.txt (#22)
    * Fix wrong ddk core version in requirements.txt
    
    * Bump version to micro 0.1.2

--- cli/aws_ddk/data/project_templates/ddk_app/{{cookiecutter.directory_name}}/app.py
+++ cli/aws_ddk/data/project_templates/ddk_app/{{cookiecutter.directory_name}}/app.py
@@ -4,6 +4,6 @@ import aws_cdk as cdk
 from {{cookiecutter.package_name}}.{{cookiecutter.stack_file_name}} import {{cookiecutter.stack_name}}
 
 app = cdk.App()
-{{cookiecutter.stack_name}}(app, "{{cookiecutter.stack_name}}", "dev")
+{{cookiecutter.stack_name}}(app, "{{cookiecutter.stack_name}}", "{{cookiecutter.environment_id}}")
 
 app.synth()
