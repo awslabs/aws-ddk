b875abc3577dcd5b33ce69a7d0a9ea4e0e38f921
Lucas Hanson <lucascchanson@gmail.com>
2022-02-28 12:02:50 -0800

    Issue #31: Adding logic to create new version only when necessary

--- docs/bin/build_docs.sh
+++ docs/bin/build_docs.sh
@@ -10,8 +10,11 @@ sphinx-build -b html -D release=${release_version} . ../release/next/api
 
 # setup new release
 cp -R ../release/next/ ../release/${release_version}
-if [ "$name" != "latest" ]
+if grep -Fq "$release_version" ../_data/versions.yaml
 then
+    echo "version: ${release_version} already exists in versions.yaml"
+else
+    echo "version: ${release_version} does not exist in versions.yaml and will be added"
     echo -e "\n- name: ${release_version}\n  root: ${release_version}" >> ../_data/versions.yaml
 fi
 popd
