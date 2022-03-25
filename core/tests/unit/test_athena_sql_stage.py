7de3a8c190d5ed202cbb81ae88611a559f4e0974
kukushking <3997468+kukushking@users.noreply.github.com>
2022-03-25 18:34:06 +0000

    Add Athena SQL stage

---
+++ core/tests/unit/test_athena_sql_stage.py
@@ -0,0 +1,44 @@
+# Copyright 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from aws_cdk.assertions import Match, Template
+from aws_ddk_core.base import BaseStack
+from aws_ddk_core.stages import AthenaSQLStage
+
+
+def test_glue_transform_stage_simple(test_stack: BaseStack) -> None:
+    AthenaSQLStage(
+        scope=test_stack,
+        id="athena-sql",
+        environment_id="dev",
+        query_string="SELECT 1;",
+        workgroup="primary",
+    )
+
+    template = Template.from_stack(test_stack)
+    template.has_resource_properties(
+        "AWS::StepFunctions::StateMachine",
+        props={
+            "DefinitionString": {
+                "Fn::Join": [
+                    "",
+                    Match.array_with(
+                        pattern=[
+                            Match.string_like_regexp(pattern="start-query-exec"),
+                        ]
+                    ),
+                ]
+            }
+        },
+    )
