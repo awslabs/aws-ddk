4bab26c24a5490dd1d5c5493c6ef335b56c148ff
lucas hanson <lucascchanson@gmail.com>
2022-05-16 10:07:03 -0700

    Minor: 0.4.0 -> 0.4.1

--- docs/_site/source/conf.py
+++ docs/_site/source/conf.py
@@ -15,7 +15,7 @@ for path in paths:
 project = "AWS DDK"
 copyright = "2022, AWS"
 author = "AWS Professional Services"
-release = "0.4.0"
+release = "0.4.1"
 
 
 # -- General configuration ---------------------------------------------------
