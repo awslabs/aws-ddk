92bfcbfa96de2c53b811dbee86f99eeef39a8d6d
Leon Luttenberger <lutleon@amazon.com>
2022-08-08 13:40:49 -0500

    Set up project metadata

--- .projenrc.js
+++ .projenrc.js
@@ -1,15 +1,20 @@
 const { awscdk } = require('projen');
 const project = new awscdk.AwsCdkConstructLibrary({
-  author: 'Leon Luttenberger',
-  authorAddress: 'lutleon@amazon.com',
+  author: 'AWS Professional Services',
+  authorAddress: 'aws-proserve-opensource@amazon.com',
+
   cdkVersion: '2.1.0',
-  defaultReleaseBranch: 'main',
-  name: 'aws-ddk',
-  repositoryUrl: 'git@github.com:awslabs/aws-ddk.git',
+  defaultReleaseBranch: 'typescript-conversion-lutleon', // TODO change this
+  name: 'aws-ddk-dev',
+  description: 'AWS DataOps Development Kit',
+  repositoryUrl: 'https://github.com/awslabs/aws-ddk',
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
 });
+
+project.gitignore.exclude('.vscode/');
+
 project.synth();
