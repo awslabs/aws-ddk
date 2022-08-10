3647747883a25b2f7236e96d77b23866a45bef1e
Leon Luttenberger <LeonLuttenberger@users.noreply.github.com>
2022-08-10 09:48:57 -0500

    feat: Set up project metadata (#136)
    * Set up project metadata
    * Remove release workflow
    * Added .vscode to gitignore

--- .projenrc.js
+++ .projenrc.js
@@ -1,15 +1,23 @@
 const { awscdk } = require('projen');
 const project = new awscdk.AwsCdkConstructLibrary({
-  author: 'Leon Luttenberger',
-  authorAddress: 'lutleon@amazon.com',
+  author: 'AWS Professional Services',
+  authorAddress: 'aws-proserve-orion-dev@amazon.com',
+
   cdkVersion: '2.1.0',
+  release: false,
   defaultReleaseBranch: 'main',
-  name: 'aws-ddk',
-  repositoryUrl: 'git@github.com:awslabs/aws-ddk.git',
+  name: 'aws-ddk-dev',
+  description: 'AWS DataOps Development Kit',
+  repositoryUrl: 'https://github.com/awslabs/aws-ddk',
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
+
+  gitignore: [
+    '.vscode/',
+  ],
 });
+
 project.synth();
---
+++ API.md
@@ -0,0 +1,39 @@
+# API Reference <a name="API Reference" id="api-reference"></a>
+
+
+
+## Classes <a name="Classes" id="Classes"></a>
+
+### Hello <a name="Hello" id="aws-ddk-dev.Hello"></a>
+
+#### Initializers <a name="Initializers" id="aws-ddk-dev.Hello.Initializer"></a>
+
+```typescript
+import { Hello } from 'aws-ddk-dev'
+
+new Hello()
+```
+
+| **Name** | **Type** | **Description** |
+| --- | --- | --- |
+
+---
+
+#### Methods <a name="Methods" id="Methods"></a>
+
+| **Name** | **Description** |
+| --- | --- |
+| <code><a href="#aws-ddk-dev.Hello.sayHello">sayHello</a></code> | *No description.* |
+
+---
+
+##### `sayHello` <a name="sayHello" id="aws-ddk-dev.Hello.sayHello"></a>
+
+```typescript
+public sayHello(): string
+```
+
+
+
+
+
