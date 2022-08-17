9e5a22bad0148c71e76614ff79f9ad140b1ba56d
Lucas Hanson <lucascchanson@gmail.com>
2022-08-17 16:15:06 -0700

    feat: Adding core.cicd (#139)
    * Adding core.cicd
    
    * updating index
    
    * Updating syntax
    
    * Resolving some build errors
    
    * changing to nullable
    
    * fixing compiler issues
    
    * removing test
    
    * adding prelim test
    
    * adding full cicd testing
    
    * fixing unused imports
    
    * test api add
    
    * formatting and removing unnecessary interfaces
    
    * eslint remediations test/*
    
    * updating codeartifact naming
    
    * removing interfaces from utils methods
    
    * capitalizing id
    
    * error checking: in progress
    
    * resolving new param pass
    
    * fixing current tests
    
    * Finalizing tests for cicd
    
    * resolving lutleon review comments
    
    * adding updated API md

---
+++ src/cicd/utils.ts
@@ -0,0 +1,94 @@
+import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
+
+export function getCodeArtifactReadPolicyStatements(
+  partition: string,
+  region: string,
+  account: string,
+  domain: string,
+  repository: string,
+): PolicyStatement[] {
+  return [
+    new PolicyStatement({
+      effect: Effect.ALLOW,
+      actions: [
+        'codeartifact:DescribeDomain',
+        'codeartifact:GetAuthorizationToken',
+        'codeartifact:ListRepositoriesInDomain',
+      ],
+      resources: [
+        `arn:${partition}:codeartifact:${region}:${account}:domain/${domain}`,
+      ],
+    }),
+    new PolicyStatement({
+      effect: Effect.ALLOW,
+      actions: [
+        'codeartifact:GetRepositoryEndpoint',
+        'codeartifact:ReadFromRepository',
+      ],
+      resources: [
+        `arn:${partition}:codeartifact:${region}:${account}:repository/${domain}/${repository}`,
+      ],
+    }),
+    new PolicyStatement({
+      effect: Effect.ALLOW,
+      actions: ['sts:GetServiceBearerToken'],
+      resources: ['*'],
+      conditions: {
+        StringEquals: {
+          'sts:AWSServiceName': 'codeartifact.amazonaws.com',
+        },
+      },
+    }),
+  ];
+}
+
+export function getCodeArtifactPublishPolicyStatements(
+  partition: string,
+  region: string,
+  account: string,
+  domain: string,
+  repository: string,
+): PolicyStatement[] {
+  return [
+    new PolicyStatement({
+      actions: [
+        'codeartifact:DescribeDomain',
+        'codeartifact:GetAuthorizationToken',
+        'codeartifact:ListRepositoriesInDomain',
+      ],
+      effect: Effect.ALLOW,
+      resources: [
+        `arn:${partition}:codeartifact:${region}:${account}:domain/${domain}`,
+      ],
+    }),
+    new PolicyStatement({
+      actions: [
+        'codeartifact:GetRepositoryEndpoint',
+        'codeartifact:ReadFromRepository',
+      ],
+      effect: Effect.ALLOW,
+      resources: [
+        `arn:${partition}:codeartifact:${region}:${account}:repository/${domain}/${repository}`,
+      ],
+    }),
+    new PolicyStatement({
+      actions: ['codeartifact:PublishPackageVersion'],
+      effect: Effect.ALLOW,
+      resources: ['*'],
+    }),
+    new PolicyStatement({
+      actions: ['sts:GetServiceBearerToken'],
+      effect: Effect.ALLOW,
+      resources: ['*'],
+      conditions: {
+        StringEquals: {
+          'sts:AWSServiceName': 'codeartifact.amazonaws.com',
+        },
+      },
+    }),
+  ];
+}
+
+export function toTitleCase(str: string): string {
+  return str.toLowerCase().replace(/\b(\w)/g, (s) => s.toUpperCase());
+}
