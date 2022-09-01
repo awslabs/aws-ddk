22155c68ba92f2663e74bc553cffdc127793d69c
lucas hanson <lucascchanson@gmail.com>
2022-09-01 11:00:16 -0700

    Minor: Updating assignment

--- src/cicd/actions.ts
+++ src/cicd/actions.ts
@@ -49,7 +49,7 @@ export function getSynthAction(props: GetSynthActionProps): CodeBuildStep {
   //   install_commands.psuh(`aws codeartifact login --tool pip --repository ${codeArtifactRepository} --domain ${codeArtifactDomain} --domain-owner ${codeArtifactDomainOwner}`);
   // }
   if (props.additionalInstallCommands != undefined && props.additionalInstallCommands.length > 0) {
-    installCommands.concat(props.additionalInstallCommands); // will need to be replaced with `npm install aws-ddk-core@${version}` when available
+    installCommands = installCommands.concat(props.additionalInstallCommands); // will need to be replaced with `npm install aws-ddk-core@${version}` when available
   }
   return new CodeBuildStep('Synth', {
     input: props.codePipelineSource,
