6f33389ea28c59d53b4901cadabc9488dc19ae11
lucas hanson <lucascchanson@gmail.com>
2022-11-18 15:15:47 -0800

    updating docs

--- core/aws_ddk_core/resources/_utils.py
+++ core/aws_ddk_core/resources/_utils.py
@@ -84,8 +84,11 @@ def pandas_sdk_layer(
     ----------
     scope: Construct
         CDK stack.
-    id : Optional[str]
+    id: Optional[str]
         Logical id of lambda layer resource in scope.
+    environment_id: Optional[str]
+        DDK environment to pull value from 
+        when using DDK config to specify layer.
     region : Optional[str]
         Name of region to lookup in. Defaults to region of CDK stack.
     version: Optional[str]
