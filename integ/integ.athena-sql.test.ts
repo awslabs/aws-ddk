fdad682ff58be8b8a7d7fa46bc72c607fdc99b90
Lucas Hanson <lucascchanson@gmail.com>
2023-02-01 09:05:05 -0800

    feat: Integration Tests: Additional Coverage (#238)
    * adding additional integration tests
    
    * updating common issues
    
    * renaming integ test appropriately

---
+++ integ/integ.athena-sql.test.ts
@@ -0,0 +1,55 @@
+import * as integration from "@aws-cdk/integ-tests-alpha";
+import * as cdk from "aws-cdk-lib";
+import * as kms from "aws-cdk-lib/aws-kms";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as tasks from "aws-cdk-lib/aws-stepfunctions-tasks";
+import { Construct } from "constructs";
+import { RequireApproval } from "aws-cdk-lib/cloud-assembly-schema";
+
+import { AthenaSQLStage } from "../src";
+import { StateMachineStageProps } from "../src/pipelines/stage";
+
+interface AthenaSqlStageTestStackProps extends StateMachineStageProps {
+  readonly queryString: string;
+  readonly workGroup?: string;
+  readonly catalogName?: string;
+  readonly databaseName?: string;
+  readonly outputLocation?: s3.Location;
+  readonly encryptionOption?: tasks.EncryptionOption;
+  readonly encryptionKey?: kms.Key;
+}
+
+class AthenaSqlStageTestStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props: AthenaSqlStageTestStackProps) {
+    super(scope, id, props);
+
+    new AthenaSQLStage(this, "Stage", {
+      ...props,
+    });
+  }
+}
+
+const app = new cdk.App();
+new integration.IntegTest(app, "AthenaSQL Stage Integration Tests", {
+    testCases: [
+      new AthenaSqlStageTestStack(app, "GlueTransformBasic", {
+        queryString: "SELECT 1",
+        workGroup: "primary",
+      }),
+    ],
+    diffAssets: true,
+    stackUpdateWorkflow: true,
+    cdkCommandOptions: {
+      deploy: {
+        args: {
+          requireApproval: RequireApproval.NEVER,
+          json: true,
+        },
+      },
+      destroy: {
+        args: {
+          force: true,
+        },
+      },
+    },
+});
---
+++ integ/integ.sqs-lambda.test.ts
@@ -0,0 +1,85 @@
+import * as integration from "@aws-cdk/integ-tests-alpha";
+import * as cdk from "aws-cdk-lib";
+import * as cloudwatch from "aws-cdk-lib/aws-cloudwatch";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as sqs from "aws-cdk-lib/aws-sqs";
+import { Construct } from "constructs";
+import { RequireApproval } from "aws-cdk-lib/cloud-assembly-schema";
+
+import { SqsToLambdaStage } from "../src";
+
+interface SqsToLambdaStageFunctionProps extends lambda.FunctionProps {
+  readonly errorsAlarmThreshold?: number;
+  readonly errorsEvaluationPeriods?: number;
+  readonly errorsComparisonOperator?: cloudwatch.ComparisonOperator;
+}
+
+interface SqsToLambdaStageTestStackProps extends cdk.StackProps {
+  readonly lambdaFunction?: lambda.IFunction;
+  readonly lambdaFunctionProps?: SqsToLambdaStageFunctionProps;
+  readonly sqsQueue?: sqs.IQueue;
+  readonly sqsQueueProps?: sqs.QueueProps;
+  readonly batchSize?: number;
+  readonly maxBatchingWindow?: cdk.Duration;
+  readonly dlqEnabled?: boolean;
+  readonly maxReceiveCount?: number;
+  readonly messageGroupId?: string;
+}
+
+class SqsToLambdaStageTestStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props: SqsToLambdaStageTestStackProps) {
+    super(scope, id, props);
+
+    new SqsToLambdaStage(this, "Stage", {
+      ...props,
+    });
+  }
+}
+
+const app = new cdk.App();
+new integration.IntegTest(app, "Sqs Lambda Stage Integration Tests", {
+    testCases: [
+      new SqsToLambdaStageTestStack(app, "SqsLambdaBasic", {
+        lambdaFunctionProps: {
+          code: lambda.Code.fromInline("def lambda_handler(event, context): return 200"),
+          handler: "lambda_function.lambda_handler",
+          memorySize: 512,
+          runtime: lambda.Runtime.PYTHON_3_9,
+        },
+      }),
+      new SqsToLambdaStageTestStack(app, "SqsLambdaFull", {
+        lambdaFunctionProps: {
+          code: lambda.Code.fromInline("def lambda_handler(event, context): return 200"),
+          handler: "lambda_function.lambda_handler",
+          memorySize: 512,
+          runtime: lambda.Runtime.PYTHON_3_9,
+          errorsAlarmThreshold: 10,
+          errorsEvaluationPeriods: 3,
+          errorsComparisonOperator: cloudwatch.ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
+        },
+        batchSize: 5,
+        messageGroupId: "dummy-group",
+        maxReceiveCount: 2,
+        dlqEnabled: true,
+        sqsQueueProps: {
+          fifo: true,
+          visibilityTimeout: cdk.Duration.minutes(5),
+        },
+      }),
+    ],
+    diffAssets: true,
+    stackUpdateWorkflow: true,
+    cdkCommandOptions: {
+      deploy: {
+        args: {
+          requireApproval: RequireApproval.NEVER,
+          json: true,
+        },
+      },
+      destroy: {
+        args: {
+          force: true,
+        },
+      },
+    },
+});
