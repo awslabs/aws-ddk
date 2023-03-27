971f35f73468deb09c5a9b86e7319a9ef0bb496b
Lucas Hanson <lucascchanson@gmail.com>
2023-03-27 15:54:20 -0700

    adding redshift data api stage (#285)

--- src/stages/index.ts
+++ src/stages/index.ts
@@ -3,6 +3,7 @@ export * from "./athena-sql";
 export * from "./databrew-transform";
 export * from "./glue-transform";
 export * from "./kinesis-s3";
+export * from "./redshift-data-api";
 export * from "./s3-event";
 export * from "./sns-lambda";
 export * from "./sqs-lambda";
