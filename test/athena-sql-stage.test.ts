0259aceead1b331a08339deb2cfe48ec8644ad7b
Anmol Gandhi <74257929+anmolsgandhi@users.noreply.github.com>
2023-05-02 17:57:57 -0400

    feat: Updating AthenaSQL stage to handle multiple queries (#317)
    * enhancement to athena-sql-stage
    
    * Supporting files
    
    * debug
    
    * debug
    
    * debug: yarn.lock sync
    
    * debug: tasks.json sync
    
    ---------
    
    Co-authored-by: Anmol Gandhi <gananmol@amazon.com>
    Co-authored-by: lucas hanson <lucascchanson@gmail.com>

--- test/athena-sql-stage.test.ts
+++ test/athena-sql-stage.test.ts
@@ -5,18 +5,51 @@ import * as tasks from "aws-cdk-lib/aws-stepfunctions-tasks";
 
 import { AthenaSQLStage } from "../src";
 
-test("AthenaToSQL stage creates State Machine", () => {
+test("AthenaToSQL stage creates State Machine single query", () => {
   const stack = new cdk.Stack();
 
   new AthenaSQLStage(stack, "athena-sql", {
-    queryString: "SELECT 1",
+    queryString: ["SELECT 1"],
     workGroup: "primary",
   });
 
   const template = Template.fromStack(stack);
   template.hasResourceProperties("AWS::StepFunctions::StateMachine", {
     DefinitionString: {
-      "Fn::Join": ["", Match.arrayWith([Match.stringLikeRegexp("Start Query Exec")])],
+      "Fn::Join": ["", Match.arrayWith([Match.stringLikeRegexp("Query Exec 0")])],
+    },
+  });
+});
+
+test("AthenaToSQL stage creates State Machine multiple query", () => {
+  const stack = new cdk.Stack();
+
+  new AthenaSQLStage(stack, "athena-sql", {
+    queryString: ["SELECT 1", "SELECT 2"],
+    workGroup: "primary",
+  });
+
+  const template = Template.fromStack(stack);
+  template.hasResourceProperties("AWS::StepFunctions::StateMachine", {
+    DefinitionString: {
+      "Fn::Join": ["", Match.arrayWith([Match.stringLikeRegexp("Query Exec 0")])],
+    },
+  });
+});
+
+test("AthenaToSQL stage creates State Machine with parallel branches", () => {
+  const stack = new cdk.Stack();
+
+  new AthenaSQLStage(stack, "athena-sql", {
+    queryString: ["SELECT 1", "SELECT 2"],
+    workGroup: "primary",
+    parallel: true,
+  });
+
+  const template = Template.fromStack(stack);
+  template.hasResourceProperties("AWS::StepFunctions::StateMachine", {
+    DefinitionString: {
+      "Fn::Join": ["", Match.arrayWith([Match.stringLikeRegexp("All Jobs")])],
     },
   });
 });
@@ -25,7 +58,7 @@ test("AthenaToSQL stage with additional properties", () => {
   const stack = new cdk.Stack();
 
   new AthenaSQLStage(stack, "athena-sql", {
-    queryString: "SELECT 1",
+    queryString: ["SELECT 1"],
     workGroup: "primary",
     encryptionOption: tasks.EncryptionOption.KMS,
     additionalRolePolicyStatements: [
@@ -41,7 +74,7 @@ test("AthenaToSQL stage with additional properties", () => {
   const template = Template.fromStack(stack);
   template.hasResourceProperties("AWS::StepFunctions::StateMachine", {
     DefinitionString: {
-      "Fn::Join": ["", Match.arrayWith([Match.stringLikeRegexp("Start Query Exec")])],
+      "Fn::Join": ["", Match.arrayWith([Match.stringLikeRegexp("Query Exec 0")])],
     },
     StateMachineName: "dummy-statemachine",
   });
@@ -73,7 +106,7 @@ test("AthenaSQLStage duplicate query string properties", () => {
   const stack = new cdk.Stack();
   expect(() => {
     new AthenaSQLStage(stack, "Stage", {
-      queryString: "SELECT 1;",
+      queryString: ["SELECT 1;"],
       queryStringPath: "$.queryString",
     });
   }).toThrowError("For this stage provide one of queryString or queryStringPath parameter, not both");
